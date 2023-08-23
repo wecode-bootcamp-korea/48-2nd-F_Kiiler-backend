ec03a908d0a3a5ea5becf057a4587aad2eba25de
jieunyang <jieunyang@jieunyangui-MacBookAir.local>
2023-08-23 18:00:46 +0900

    mod

--- src/models/user.dao.js
+++ src/models/user.dao.js
@@ -18,19 +18,18 @@ const createUser = async (email, password) => {
 const getUserByEmail = async (email) => {
   const [user] = await AppDataSource.query(
     `
-      SELECT 
-      id, 
-      email,
-      password,
-      nickname,
-      agreement_id,
-      agree_app,
-      agree_sms,
-      agree_email, 
-      point
-
-      FROM users
-      WHERE email = ?
+    SELECT
+    id,
+    email,
+    password,
+    nickname,
+    agreement_id,
+    agree_app,
+    agree_sms,
+    agree_email,
+    point
+FROM users
+WHERE email = ?
     `,
     [email]
   );
@@ -40,19 +39,18 @@ const getUserByEmail = async (email) => {
 const getUserById = async (id) => {
   const [user] = await AppDataSource.query(
     `
-      SELECT 
-      id, 
-      email,
-      password,
-      nickname,
-      agreement_id,
-      agree_app,
-      agree_sms,
-      agree_email, 
-      point
-      
-      FROM users
-      WHERE id = ?
+    SELECT
+    id,
+    email,
+    password,
+    nickname,
+    agreement_id,
+    agree_app,
+    agree_sms,
+    agree_email,
+    point
+FROM users
+WHERE id = ?
     `,
     [id]
   );
