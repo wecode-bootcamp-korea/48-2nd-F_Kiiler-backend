eb813d3faf9d8ecebbaf240049df9fd045bb6eea
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2023-08-28 17:22:55 +0900

    수정

--- src/controllers/sell.controller.js
+++ src/controllers/sell.controller.js
@@ -1,4 +1,5 @@
 const sellDao = require('../models/sell.dao');
+const buyDao = require('../models/buy.dao');
 const { catchAsync } = require('../utils/error');
 
 const getSellList = catchAsync(async (req, res) => {
@@ -7,6 +8,8 @@ const getSellList = catchAsync(async (req, res) => {
 
   if (size) {
     const result = await sellDao.getSellSizeList(productId, size);
+    result2 = await buyDao.getBuySize(productId, size);
+    result[0]['buyPrice'] = result2[0]['buyPrice'];
     res.status(201).json({ data: result });
   } else {
     const result = await sellDao.getSellList(productId);
