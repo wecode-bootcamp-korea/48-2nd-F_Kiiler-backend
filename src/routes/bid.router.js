f20d1852b6077522bcfc454861932c4aee537b8a
Jjunjiii’ <drum1701@gmail.com>
2023-08-31 17:18:08 +0900

    bidBuy 관련 추가

--- src/routes/bid.router.js
+++ src/routes/bid.router.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const { loginRequired } = require('../utils/auth');
+
 const bidSellController = require('../controllers/bid.sell.controller');
+const bidBuyController = require('../controllers/bid.buy.controller');
 const bidRouter = express.Router();
 
-bidRouter.post('/sell', bidSellController.insertBidSellWaiting);
-bidRouter.get('/:id', bidSellController.getBidSell);
-bidRouter.post('/payment', bidSellController.insertBidSellOrOrder);
+bidRouter.post('/sell', loginRequired, bidSellController.insertBidSellWaiting);
+bidRouter.get('/:id', loginRequired, bidSellController.getBidSell);
+bidRouter.post(
+  '/payment',
+  loginRequired,
+  bidSellController.insertBidSellOrOrder
+);
 
+bidRouter.post('/buy', loginRequired, bidBuyController.insertBidBuyWaiting);
+bidRouter.get('/:id', loginRequired, bidBuyController.getBidBuy);
+bidRouter.post('/payment', loginRequired, bidBuyController.insertBidBuyOrOrder);
 module.exports = { bidRouter };
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,6 @@ const { listRouter } = require('./list.router');
 const { sellRouter } = require('./sell.router');
 const { userRouter } = require('./user.router');
 
-express = require('express');
 const routes = express.Router();
 
 routes.use('/user', userRouter);
